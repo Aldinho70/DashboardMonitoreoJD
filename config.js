e133ea7368a86300e89a63227da6bb5f8847a9e8
Jesus Manuel Bobadilla Villagrana <jesus.bobadilla@metricamovil.com>
2025-06-21 17:11:41 -0600

    first commit

---
+++ config.js
@@ -0,0 +1,33 @@
+export const env = {
+    token: "e1749cb44770905014c9e8957aff9e8952E1331623A334EC024A9B0DD2274367C12C2743",
+    GroupsUnit: 'true',
+        gruposInteres1: { /*El numero recomendado de de grupos son 5 por cada lado */
+            '03 CARGAS A SEGUIR':'A SEGUIR',
+            '05 TANQUES TICSA':'TANQUES',
+            '04 UNIDADES GRAL AFUERA': 'En transito',
+        },
+        gruposInteres2: {
+            '01 UNIDADES VACIAS TFS/TCS': 'Vacias',
+            '02 UNIDADES CARGADAS TFS/TCS': 'Cargadas',
+            'UNIDADES EN CLIENTE TCS/TFS': 'Con cliente',
+        },
+        StatusUnit: 'false',
+        statusInteres: {
+            'VACIO': 'VACIO',
+            'CARGADO': 'CARGADO',
+            'ESPERA_CARGA': ' CARGA',
+            'ESPERA_DESCARGA': 'DESCARGA',
+        }, 
+    Name_cajas: '',
+    grupoInteres_modules: {},
+    Temperature: 'false',
+    Estatus: 'false',
+    map:'false',
+    img: { /* Opcional */
+        
+    },
+    bootstrap: { /* Opcional */ }
+}
+
+// Usuario: TICSA/TRAFUSA DESARROLLOS
+// Contrasena: Ticfusa-2024
---
+++ config2.js
@@ -0,0 +1,37 @@
+export const env = {
+    token: "9bcd8acb53fc9693c133579e22e81c0bD6514E2BB6B1B4ED3F2898EF511649240FCCB04E",
+    GroupsUnit: 'true',
+    gruposInteres1: { 
+        '02/CARGAS A SEGUIR':'A SEGUIR',
+        'GAS TANQUES TICSA':'TANQUES',
+        'TRACTOS SIN REPORTAR':'TANQUES SIN REPORTAR',
+        'UNIDADES CARGADAS TFS/TCS':'TFS/TCS',
+        '01/GR-AREA 2':'GR A2',
+    },
+    gruposInteres2: {  },
+    StatusUnit: 'false',
+        statusInteres: {
+            'VACIO': 'VACIO',
+            'CARGADO': 'CARGADO',
+            'ESPERA_CARGA': ' CARGA',
+            'ESPERA_DESCARGA': 'DESCARGA',
+            'GR general':'GR general'
+        }, 
+    Temperature: 'false',
+    Estatus: 'true',
+    map:'false',
+    img: { /* Opcional */
+        'A SEGUIR': './img/ticsa.png',
+        'TANQUES': './img/ticsa.png',
+        'TANQUES SIN REPORTAR': './img/ticsa.png',
+        'TFS/TCS': './img/ticsa.png',
+        "GR A2": './img/GR.png',
+    },
+    bootstrap:{ /* Opcional */ 
+        
+    }
+    
+}
+
+// Usuario: TICSA/TRAFUSA DESARROLLOS
+// Contrasena: Ticfusa-2024
